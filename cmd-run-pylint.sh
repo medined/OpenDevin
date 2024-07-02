197dd24a596a1829c4fad4b60f32860aa626c54b
David Medinets <david.medinets@gmail.com>
2024-07-02 03:23:49 -0400

    update pylint script to allow python v3.10 keywords

--- cmd-run-pylint.sh
+++ cmd-run-pylint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pylint --output-format=colorized --reports=y opendevin | tee pylint.hidden.log
+pylint --py-version=3.10 --output-format=colorized --reports=y opendevin | tee pylint.hidden.log
 echo "********************"
 echo "See pylint.hidden.log for lint messages"
-
